7db2556c78f03aacb5aaf38e86da53d6300c7a23
erico <njukieric233@gmail.com>
2025-10-08 10:46:26 +0300

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
-<script setup></script>
-
 <template>
-  <section class="h-[200vh] bg-black text-white flex items-center justify-center">
-    <h1 ref="title" class="text-6xl font-bold">Smooth Scroll + GSAP ✨</h1>
-  </section>
+  <div>
+    <TriggerSection />
+  </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
-import { gsap } from "@/plugins/gsap";
+import TriggerSection from './components/trigger-section.vue';
 
-const title = ref(null);
 
-onMounted(() => {
-  gsap.from(title.value, {
-    y: 100,
-    opacity: 0,
-    duration: 1.2,
-    ease: "power4.out",
-  });
-});
 </script>
 
-
-<style scoped></style>
---
+++ src/components/trigger-section.vue
@@ -0,0 +1,108 @@
+<template>
+  <section>
+    <!-- trigger section Scroll left -->
+    <div class="trigger_col" id="scroll-left">
+      <div class="trigger_col_slider">
+        <div class="trigger_col_slider_media">
+          <img
+            src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759844178/wmremove-transformed_zihmhw.png"
+            alt="picture"
+          />
+        </div>
+        <div class="trigger_col_slider_media">
+          <img
+            src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759844178/wmremove-transformed_zihmhw.png"
+            alt="picture"
+          />
+        </div>
+      </div>
+
+      <div class="trigger_col_nav">
+        <div class="trigger_col_nav_container b-absolute--center">
+          <span>Chapters</span>
+          <ul>
+            <li class="trigger_col_nav_item">GRIND</li>
+            <li class="trigger_col_nav_item">SLIDE</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="trigger_col" id="scroll-right">
+      <div class="trigger_col_container">
+        <div class="trigger_col_container_block">
+          <h1>Grind</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sit! Pariatur corporis
+            maiores placeat odit dolores minus soluta nihil distinctio recusandae at, amet, nam,
+            blanditiis beatae fugit atque dolorum fuga?
+          </p>
+          <div class="trigger_col_block_cover">
+            <div class="trigger_col_block_cover_medias">
+                <img
+                  src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759840762/   gaelle-marcel-f6ZQIQ6wAmE-unsplash_xicqhv.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759840762/   hector-o-connor-XSknXjyCVcs-unsplash_vitqe5.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759840876/   alvan-nee-G0lfUsch6nU-unsplash_gmdxcw.jpg"
+                  alt=""
+                />
+            </div>
+            <div class="trigger_cols_block_texts b-absolute--center">
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+            </div>
+          </div>
+          <div class="trigger_col_block_cover_bg">
+            <img src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759845428/raquel-pedrotti-AHgpNYkX9dc-unsplash_jqz1gr.jpg" alt="">
+          </div>
+        </div>
+        <div class="trigger_col_container_block">
+          <h1>Slide</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sit! Pariatur corporis
+            maiores placeat odit dolores minus soluta nihil distinctio recusandae at, amet, nam,
+            blanditiis beatae fugit atque dolorum fuga?
+          </p>
+          <div class="trigger_col_block_cover">
+            <div class="trigger_col_block_cover_medias">
+                <img
+                  src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759840762/   gaelle-marcel-f6ZQIQ6wAmE-unsplash_xicqhv.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759840762/   hector-o-connor-XSknXjyCVcs-unsplash_vitqe5.jpg"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759840876/   alvan-nee-G0lfUsch6nU-unsplash_gmdxcw.jpg"
+                  alt=""
+                />
+            </div>
+            <div class="trigger_cols_block_texts b-absolute--center">
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+                <h2>Skate * Skate</h2>
+            </div>
+          </div>
+          <div class="trigger_col_block_cover_bg">
+            <img src="https://res.cloudinary.com/dvoq5mos7/image/upload/v1759845428/raquel-pedrotti-AHgpNYkX9dc-unsplash_jqz1gr.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<script setup></script>
